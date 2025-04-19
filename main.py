42cbb23ea420d9e527b66ece1d3c9ee24cb5a7c1
cynerplay <150257505+cynerplay@users.noreply.github.com>
2025-04-19 17:49:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import pygame as pg
+from pygame import *
+from player import *
+from blocks import *
+
--- player.py
+++ player.py
@@ -29,3 +29,109 @@ ANIMATION_JUMP_LEFT = [('%s/mario/jl.png' % ICON_DIR, 0.1)]
 ANIMATION_JUMP_RIGHT = [('%s/mario/jr.png' % ICON_DIR, 0.1)]
 ANIMATION_JUMP = [('%s/mario/j.png' % ICON_DIR, 0.1)]
 ANIMATION_STAY = [('%s/mario/0.png' % ICON_DIR, 1)]
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+
+        # Speed player
+        self.xvel = 0 # speed of movement 0 - stay  place
+        self.yvel = 0 #speed of vertical movement
+
+        #Start position for player
+        self.startX = x
+        self.startY = y
+
+        self.on_ground = False # on grass is me?
+
+        self.image = pg.Surface((WIDTH, HEIGHT))
+        self.image.fill(pg.Color(COLOR))
+        self.image.set_colorkey(pg.Color(COLOR))
+
+        self.rect = pg.Rect(x, y, WIDTH, HEIGHT)
+
+        # ANIMATION
+        boltAnim = []
+        for a in ANIMATION_RIGHT:
+            boltAnim.append((a, ANIMATION_DELAY))
+        self.boltAnimRight = pyganim.PygAnimation(boltAnim)
+        self.boltAnimRight.play()
+
+        boltAnim = []
+        for a in ANIMATION_LEFT:
+            boltAnim.append((a, ANIMATION_DELAY))
+        self.boltAnimLeft = pyganim.PygAnimation(boltAnim)
+        self.boltAnimLeft.play()
+
+        self.boltAnimStay = pyganim.PygAnimation(ANIMATION_STAY)
+        self.boltAnimStay.play()
+        self.boltAnimStay.blit(self.image, (0, 0)) # in default stay
+
+        self.boltAnimJumpLeft = pyganim.PygAnimation(ANIMATION_JUMP_LEFT)
+        self.boltAnimJumpLeft.play()
+
+        self.boltAnimJumpRight = pyganim.PygAnimation(ANIMATION_JUMP_RIGHT)
+        self.boltAnimJumpRight.play()
+
+        self.boltAnimJump = pyganim.PygAnimation(ANIMATION_JUMP)
+        self.boltAnimJump.play()
+
+    def update(self, left, right, up, platforms):
+
+        if up:
+            if self.on_ground:
+                self.yvel = -JUMP_POWER
+            self.image.fill(pg.Color(COLOR))
+            self.boltAnimJump.blit(self.image, (0, 0))
+
+        if left:
+            self.xvel = -MOVE_SPEED
+            self.image.fill(pg.Color(COLOR))
+
+            if up:
+                self.boltAnimJumpLeft.blit(self.image, (0, 0))
+            else:
+                self.boltAnimLeft.blit(self.image, (0, 0))
+
+            if right:
+                self.xvel = MOVE_SPEED
+                self.image.fill(pg.Color(COLOR))
+
+                if up:
+                    self.boltAnimJumpRight.blit(self.image, (0, 0))
+                else:
+                    self.boltAnimRight.blit(self.image, (0, 0))
+
+            if not ( left or right ):
+                self.xvel = 0
+                if not up:
+                    self.image.fill(pg.Color(COLOR))
+                    self.boltAnimStay.blit(self.image, (0, 0))
+
+            if not self.on_ground:
+                self.yvel += GRAVITY
+
+            self.on_ground = False
+
+            self.rect.y += self.yvel
+            self.collide(0, self.yvel, platforms)
+
+            self.rect.x += self.xvel
+            self.collide(self.xvel, 0, platforms)
+
+        def collide(self, xvel, yvel, platforms):
+            for p in platforms:
+                if pg.sprite.collide_rect(self, p):
+
+                    if xvel > 0:
+                        self.rect.right = p.rect.left
+                    if xvel < 0:
+                        self.rect.left = p.rect.right
+
+                    if yvel > 0:
+                        self.rect.bottom = p.rect.top
+                        self.on_ground = True
+                        self.yvel = 0
+                    if yvel < 0:
+                        self.rect.top = p.rect.bottom
+                        self.yvel = 0
